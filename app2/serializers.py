78d7994eaf006669103943541a074554a366fa34
Erhan Cokimen <erhanburhanudin@gmail.com>
2022-07-23 00:25:05 +0700

    change return response viewset Person via serializer

--- app2/serializers.py
+++ app2/serializers.py
@@ -1,8 +1,7 @@
+from .models import (Person, School)
 from rest_framework import serializers
 from django.db import transaction
-
 # import models
-from .models import (Person, School)
 
 
 class PersonSerialiser(serializers.ModelSerializer):
@@ -13,10 +12,23 @@ class PersonSerialiser(serializers.ModelSerializer):
 
     def create(self, validated_data):
         schools = self.context.get('request').data.get('schools')
-        import pdb
-        pdb.set_trace()
         person = Person.objects.create(**validated_data)
-        return super().create(validated_data)
+        for x in schools:
+            instance_school = School()
+            instance_school.school_name = x.get("school_name")
+            instance_school.school_level = x.get("school_level")
+            instance_school.person = person
+            instance_school.save()
+        return person
+
+    def to_representation(self, instance):
+        schools = School.objects.filter(pk=instance.id)
+        return {
+            "firstname": instance.firstname,
+            "lastname": instance.lastname,
+            "birth_date": instance.birth_date,
+            "schools": [{"school_name": x.school_name, "school_level": x.school_level} for x in schools]
+        }
 
 
 class SchoolSerializer(serializers.ModelSerializer):
